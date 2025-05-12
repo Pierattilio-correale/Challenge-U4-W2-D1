7eac84bb5cbac7f6190e2cb70de40ded5ecf57a2
Pierattilio-correale <pierattiliocorreale@yahoo.it>
2025-05-12 16:54:22 +0200

    Update ProvaConto.java

--- src/esercizio3/ProvaConto.java
+++ src/esercizio3/ProvaConto.java
@@ -3,13 +3,13 @@ package esercizio3;
 public class ProvaConto {
     public static void main(String[] args) {
 ContoCorrente conto = new ContoCorrente("Luca" , 200);
-//try {
-//    conto.preleva(300);
-//
-//}catch (BancaException e){
-//
-//    System.out.println(e.getMessage());
-//}
+try {
+    conto.preleva(300);
+
+}catch (BancaException e){
+
+    System.out.println(e.getMessage());
+}
 ContoOnLine contoOn = new ContoOnLine("Luca", 22000 , 20000 );
 try {
     contoOn.preleva(21000);
